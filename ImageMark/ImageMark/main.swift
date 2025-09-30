d52d22177f5d375e3754491aa34642f77f62c539
Alexander6Shevtsov <axrsvr@gmail.com>
2025-09-30 12:01:23 +0300

    добавлена структура изображения и код проверки

--- ImageMark/ImageMark/main.swift
+++ ImageMark/ImageMark/main.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+struct ProcessedImage {
+	let fileName: String
+	let status: String
+}
+
+let imageService = ImageService()
+let imageNames = ["image1.png", "image2.jpg", "image3.jpg", "image4.jpg"]
+
+DispatchQueue.main.async {
+	imageService.processImagesInParallel(imageNames: imageNames) { result in
+		print("Асинхронно обработано изображений: \(result.count)")
+		print("Результаты: \(results.map { $0.fileName })")
+	}
+	
+	print("Начинаем загружать и обрабатывать изображения")
+}
