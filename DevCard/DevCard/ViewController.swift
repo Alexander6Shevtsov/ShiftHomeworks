fa3f3100101329a83da9f68cfa393ca13b28b762
Alexander6Shevtsov <axrsvr@gmail.com>
2025-10-03 10:25:04 +0300

    создан

---
+++ DevCard/DevCard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DevCard
+//
+//  Created by Alexander Shevtsov on 02.10.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
