d4a596086756de328392adf5e96c943fa0f0fdb7
Alexander6Shevtsov <axrsvr@gmail.com>
2025-10-08 22:11:39 +0300

    удалил main, настроил root

---
+++ CollectionApp/CollectionApp/CatalogViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CatalogViewController.swift
+//  CollectionApp
+//
+//  Created by Alexander Shevtsov on 08.10.2025.
+//
+
+import UIKit
+
+final class CatalogViewController: UIViewController {
+	
+	
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		title = "Catalog"
+		view.backgroundColor = .systemBackground
+	}
+}
