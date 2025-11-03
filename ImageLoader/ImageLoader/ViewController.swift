b82d736e7c465bf02d10a7e8f30d1e3d6d47a03b
Alexander6Shevtsov <axrsvr@gmail.com>
2025-11-03 18:15:37 +0300

    создан, настроен

---
+++ ImageLoader/ImageLoader/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  ImageLoader
+//
+//  Created by Alexander Shevtsov on 03.11.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+	}
+}
